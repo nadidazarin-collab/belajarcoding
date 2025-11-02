deb89dba6b457c16a0dc1593e57bdfb594b06e87
Nadida Zarin Mahira <mahiranadidazarin@gmail.com>
2025-11-02 20:59:26 +0700

    soal1

---
+++ soal1.java
@@ -0,0 +1,15 @@
+public class belajar {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+            if(i %2== 0){
+                System.out.println(+ i+" - Genap");
+                
+            }else {
+                System.out.println(+ i+" - Ganjil");
+               
+            }
+            
+        }
+    }
+    
+}
